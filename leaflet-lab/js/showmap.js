570ae656c1a9167e068544bdf3899b4cae0c8eb2
Kim Sundeen <kim.h.sundeen@gmail.com>
2017-02-19 22:54:06 -0600

    Created sequencing proportional symbols
    Added sequenc chart to scroll through enrollment attributes.

--- leaflet-lab/js/showmap.js
+++ leaflet-lab/js/showmap.js
@@ -22,24 +22,6 @@ function showMap(){
     getAjaxData(mymap);
 }
 
-//// creates a popups for each feature using a loop
-//function onEachFeature(feature, layer) {
-//    //no property named popupContent; instead, create html string with all properties
-//    var popupContent = "";
-//    
-//    if (feature.properties && feature.properties.year) {
-//        layer.bindPopup(feature.properties.description);
-//        
-//        // Loop to add feature property names and values to html string
-//        // --adds all field values to popup. Could use if wanting to show data for all years
-//        // --if data are structured like that!
-//        for (var property in feature.properties){
-//            popupContent += "<p>" + property + ": " + feature.properties.description + "</p>";
-//        }
-//        layer.bindPopup(popupContent);
-//    };
-//};
-
 // calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
     // Scale factor to adjust symbol size evenly
@@ -51,10 +33,14 @@ function calcPropRadius(attValue) {
     return radius;
 };
 
-// Step 3: Add circle markers for point features to the map
-function pointToLayer(feature, latlng) {
-    // symbolize 1 attribute
-    var attribute = "fall_2012";
+// Add circle markers for point features to the map
+function pointToLayer(feature, latlng, attributes) {
+    // symbolize current attribute in attributes array
+    var attribute = attributes[0];
+    console.log(attribute);
+    
+//    // symbolize 1 attribute
+//    var attribute = "fall_2012";
     
     // create marker options
     var options = {
@@ -109,27 +95,125 @@ function pointToLayer(feature, latlng) {
     return layer;    
     };
 
-function createPropSymbols(data, map) {   
-    // create a Leaflet GeoJSON layer and add it to the map
+function createPropSymbols(data, map, attributes) {   
+    // create a Leaflet GeoJSON layer and adds it to the map
     L.geoJson(data, {
-        pointToLayer: pointToLayer // Takes function pointToLayer
+        pointToLayer: function(feature, latlng) {
+            return pointToLayer(feature, latlng, attributes);
+        }
     }).addTo(map);
 };
 
+// Resize proportional symbols according to new attribute values
+function updatePropSymbols(map, attribute) {
+    map.eachLayer(function(layer) {
+       // checks for existence of feature in layer & of a selected attribute in layer
+       if (layer.feature && layer.feature.properties[attribute]) {
+           // update layer style & popup   
+           // access feature properties
+           var props = layer.feature.properties;
+           
+           // update each feature's radius based on new attribute vals
+           var radius = calcPropRadius(props[attribute]);
+           layer.setRadius(radius);
+           
+           // add "University" text to popup content string
+           var popupContent = "<p><b> University:</b> " + props.university + "</p>";
+           
+           // add formatted year value attribute to panel content string
+           var year = attribute.split("_")[1];
+           
+           // replace layer popup
+           layer.bindPopup(popupContent, {
+               offset: new L.Point(0,-radius)
+           });
+       };
+    });
+};
+
 // create sequence controls
-function createSequenceControls(map) {
+function createSequenceControls(map, attributes) {
     // append a range-slider to dom
     $('#panel').append('<input class="range-slider" type="range">');
+    $('#panel').append('<button class="skip" id="reverse">Reverse</button>');
+    $('#panel').append('<button class="skip" id="forward">Skip</button>');    
+    $('#reverse').html('<img src="img/reverse.png">');
+    $('#forward').html('<img src="img/forward.png">');
+    
+    // set slider attributes
+    $('.range-slider').attr({
+        max: 6,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+    
+    // add click listener events for buttons
+    $('.skip').click(function() {
+        // get old index value
+        var index = $('.range-slider').val();
+        
+        // increment/decrement depending on button clicked
+        if ($(this).attr('id') == 'forward') {
+            index++;
+            
+            // if passed last attribute, wrap to beginning again // shorthand if-else: "if index is greater than 6, return 0, else return index"
+            index = index > 6 ? 0 : index;    
+        } else if ($(this).attr('id') == 'reverse') {
+            index--;
+            
+            // if passed last attribute, wrap to beginning again // shorthand if-else: "if index is less than 0, return 6, else return index"
+            index = index < 0 ? 6 : index;
+        };
+        
+        // update slider with value
+        $('.range-slider').val(index);
+        
+        // pass new attribute to update proportional symbol sizes
+        updatePropSymbols(map, attributes[index]);
+    });
+    
+    // input listener for slider
+    $('.range-slider').on('input', function() {
+        // get new index value of sequence
+        var index = $(this).val();
+        console.log(index);
+        
+        // pass new attribute to update proportional symbol sizes
+        updatePropSymbols(map, attributes[index]);
+    })
+};
+
+// Builds attribute array
+function processData(data) {
+    var attributes = [];
+    
+    // properties of 1st feature in dataset
+    var properties = data.features[0].properties;
+    console.log("Properties: ", properties);
+    
+    // loop through all attributes & populate list
+    for (var attribute in properties) {
+        // only take attributes with enrollment values, searching for header with "enroll" in the value
+        if (attribute.indexOf("fall") > -1) {
+            attributes.push(attribute);
+        }
+    }
+    
+    console.log(attributes);
+    return attributes;
 }
 
-// Step 2: Import JSON data
+// Import JSON data
 function getAjaxData(map){
     // load the data
     $.ajax("data/university_enrollments_over10k.geojson", {
         dataType: "json",
         success: function(response) {
-            createPropSymbols(response, map);
-            createSequenceControls(map);
+            // hold data attributes as an array
+            var attributes = processData(response);
+            createPropSymbols(response, map, attributes);
+            createSequenceControls(map, attributes);
         }
     });
 };
